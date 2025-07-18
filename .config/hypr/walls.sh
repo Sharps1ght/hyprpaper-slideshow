b6f67902725e05bcecd26a164c5b97e402aa05e5
Sharps1ght <162005925+Sharps1ght@users.noreply.github.com>
2025-07-18 21:27:50 +0700

    Update walls.sh

--- .config/hypr/walls.sh
+++ .config/hypr/walls.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-# Directory where wallpapers are pulled from 
+# Directory where wallpapers are pulled from (need to be set manually)
 wallpaperDir="$HOME/.config/hypr/Wallpapers/"
 # Check which wallpapers are currently in use
 currentWall=$(hyprctl hyprpaper listactive)
 # List all monitors connected
 monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+# Amount of seconds that need to pass for wallpaper to be changed on ONE monitor
+# Multiply by amount of monitors you have to find out how much time will pass before  a wallpaper will be swapped on THE monitor
+timer=30
 
 # Start an infinite loop
 while true; do
 	# Go through all of the connected monitors
 	for display in $monitors; do
 		# Call out the selected monitor
-		echo "Changing $display..."	
+		echo "Changing $display..."
 		# Pick a random wallpaper from directory (not fool-proof, theoretically can select ANY file)
 		wallpaper=$(find "$wallpaperDir" -type f ! -name "$(basename "$currentWall")" | shuf -n 1)
 		# Call out the selected wallpaper
@@ -22,9 +25,9 @@ while true; do
 		# Result
 		echo "$display is set to $(basename $wallpaper)"
 		# Timer
-		sleep 30
+		sleep $timer
 	done
 	# Dunno why but unload all of the wallpapers
-	# (it won't remove any currently active wallpapers from the monitors)
+	# It won't remove any currently active wallpapers from the monitors
 	hyprctl hyprpaper unload all
 done
